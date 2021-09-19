a59c22fab722166481cc0eadbde5023594ae9d13
Sergey Pikhota <sergey.pikhota@protonmail.com>
2021-09-19 23:43:26 +0200

    Add tests for different collections

--- Assets/Foreach.cs
+++ Assets/Foreach.cs
@@ -6,49 +6,63 @@ using UnityEngine.Profiling;
 
 public class Foreach : MonoBehaviour
 {
-	void Start()
+	private void Start()
 	{
 		Profiler.logFile = "Profile.raw";
 		Profiler.enableBinaryLog = true;
 		Profiler.enabled = true;
 	}
 
-    void Update()
+    private void Update()
     {
         int number = 0;
+
         var list = new List<int>(Enumerable.Range(0, 200).Select(i => i + Random.Range(-40, 50)));
-
-		Profiler.BeginSample("For loop", this);
-        for (int i = 0; i < list.Count; i++)
-        {
-            number += list[i];
-        }
-		Profiler.EndSample();
-
+        
 		Profiler.BeginSample("Foreach on List", this);
         foreach (int i in list)
         {
             number += i;
         }
 		Profiler.EndSample();
-
-        var ilist = (IList<int>) list;
-		Profiler.BeginSample("Foreach on IList", this);
-        foreach (int i in ilist)
-        {
-            number += i;
-        }
-		Profiler.EndSample();
-
-		var array = Enumerable.Range(0, 200).ToArray();
+		
+		int[] array = Enumerable.Range(0, 200).ToArray();
 		Profiler.BeginSample("Foreach on array", this);
         foreach (int i in array)
         {
             number += i;
         }
 		Profiler.EndSample();
-        
+
+        var ilist = (IList<int>) list;
+		// Profiler.BeginSample("Foreach on IList", this);
+        foreach (int i in ilist)
+        {
+            number += i;
+        }
+		// Profiler.EndSample();
+
+		var dictionary = new Dictionary<int, int>();
+		for (int i = 0; i < 200; i++)
+		{
+			dictionary.Add(i, Random.Range(-50, 50));
+		}
+
+		Profiler.BeginSample("Foreach on Dictionary", this);
+		foreach (KeyValuePair<int,int> keyValuePair in dictionary)
+		{
+			number += keyValuePair.Value;
+		}
+		Profiler.EndSample();
+
+		var hashMap = new HashSet<int>(Enumerable.Range(0, 200));
+		Profiler.BeginSample("Foreach on HashSet", this);
+		foreach (int i in hashMap)
+		{
+			number += i;
+		}
+		Profiler.EndSample();
+		
         Debug.Log(number);
-		Debug.Break();
     }
 }
